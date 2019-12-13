fe427a38d826b1d05491692a5caca6faa291a45e
danielhellgren <daniel__Hellgren@hotmail.com>
2019-12-13 20:35:15 +0100

    Test cases reveal no error but wrong answer still

--- Day13/cart.py
+++ Day13/cart.py
@@ -1,8 +1,11 @@
 class Cart:
+    #cart directions have to be in order
+    # UP RIGHT DOWN LEFT as de/incrementing is used for turning
     CART_DIR_UP = 0
     CART_DIR_DOWN = 2
     CART_DIR_LEFT = 3
     CART_DIR_RIGHT = 1
+
     ROT_LEFT = -1
     ROT_RIGHT = 1
 
@@ -12,54 +15,80 @@ class Cart:
         self.intersection_mode = 0
         print("Added a cart at pos: " + str(pos[0]) + ", " + str(pos[1]))
     
+    #Rotate cart based on dir
+    # for instance UP - rotate right -> Right
     def rotate(self, rot_dir):
         new_dir = (self.direction + rot_dir) % 4
         self.direction = new_dir
         
-
+    #turns cart if it should
+    #ie on an intersection or turn
     def turn(self, cur_pos_type):
       
+        #on an intersection
+        #intersection turning rules
         if(cur_pos_type == "+"):
+
             if(self.intersection_mode == 0):
                 self.rotate(Cart.ROT_LEFT)
             if(self.intersection_mode == 2):
                 self.rotate(Cart.ROT_RIGHT)
-            self.intersection_mode = (self.intersection_mode + 1) % 3
 
+            self.intersection_mode = (self.intersection_mode + 1) % 3
+        
         elif(cur_pos_type == "/"):
-            if self.direction == Cart.CART_DIR_UP or self.direction == Cart.CART_DIR_DOWN:
-                self.rotate(Cart.ROT_RIGHT)
-            else:
-                self.rotate(Cart.ROT_LEFT)
+            #if coming from above or below the cart will turn right
+            #ternary operator
+            self.rotate(Cart.ROT_RIGHT) if (self.direction == Cart.CART_DIR_UP or self.direction == Cart.CART_DIR_DOWN) else self.rotate(Cart.ROT_LEFT)
+            
+            #if self.direction == Cart.CART_DIR_UP or self.direction == Cart.CART_DIR_DOWN:
+            #    self.rotate(Cart.ROT_RIGHT)
+            #else:
+            #    self.rotate(Cart.ROT_LEFT)
 
         elif(cur_pos_type == "\\"):
-            if self.direction == Cart.CART_DIR_UP or self.direction == Cart.CART_DIR_DOWN:
-                self.rotate(Cart.ROT_LEFT)
-            else:
-                self.rotate(Cart.ROT_RIGHT)
+            #if coming from above or below the cart will turn left
+            self.rotate(Cart.ROT_LEFT) if (self.direction == Cart.CART_DIR_UP or self.direction == Cart.CART_DIR_DOWN) else self.rotate(Cart.ROT_RIGHT)
+
+            #if self.direction == Cart.CART_DIR_UP or self.direction == Cart.CART_DIR_DOWN:
+            #    self.rotate(Cart.ROT_LEFT)
+            #else:
+            #    self.rotate(Cart.ROT_RIGHT)
 
 
+    #changes carts position based on cart direction
     def move(self, track_matrix):
+
         pos_x = self.pos[0]
         pos_y = self.pos[1]
+        
         if self.direction == Cart.CART_DIR_UP:
             pos_y -= 1
-        if self.direction == Cart.CART_DIR_DOWN:
+        elif self.direction == Cart.CART_DIR_DOWN:
             pos_y += 1
-        if self.direction == Cart.CART_DIR_LEFT:
+        elif self.direction == Cart.CART_DIR_LEFT:
             pos_x -= 1
-        if self.direction == Cart.CART_DIR_RIGHT:
+        elif self.direction == Cart.CART_DIR_RIGHT:
             pos_x += 1
 
         self.pos = (pos_x, pos_y)
 
         self.turn(track_matrix[pos_y][pos_x])
         
-    def print(self):
+    def printCart(self):
         print(" ("+ str(self.pos[0]) + ", " + str(self.pos[1]) + ") ")
 
     def getPosY(self):
         return self.pos[1]
+
+    def getPosX(self):
+        return self.pos[0]
+
+    def getIntersectionMode(self):
+        return self.intersection_mode
+
+    def getDirection(self):
+        return self.direction
 
     def getPos(self):
         return self.pos
---
+++ Day13/solutionTest.py
@@ -0,0 +1,118 @@
+import unittest
+import solution
+from cart import Cart
+
+class SolutionTest(unittest.TestCase):
+
+    def init(self):
+        return solution.init()
+
+    def tick(self, tracks, carts, steps):
+        for _ in range(steps):
+            solution.moveCarts(tracks, carts)
+
+
+    def test_loader(self):
+
+        tracks, carts = self.init()
+        #CHECK INIT
+        #amount of rows
+        self.assertEqual(len(tracks), 150)
+        #col
+        self.assertEqual(len(tracks[0]), 151)
+        #amount of carts loaded
+        self.assertEqual(len(carts), 17)
+
+    def test_movement(self):
+
+        tracks, carts = self.init()
+
+        #intial positions
+        self.assertEqual(carts[0].getDirection(), Cart.CART_DIR_RIGHT)
+        self.assertEqual(carts[0].getPos(), (60, 4))
+
+        self.assertEqual(carts[9].getDirection(), Cart.CART_DIR_UP)
+        self.assertEqual(carts[9].getPos(), (106, 74))
+
+        self.assertEqual(carts[10].getDirection(), Cart.CART_DIR_LEFT)
+        self.assertEqual(carts[10].getPos(), (35, 95))
+
+        self.assertEqual(carts[16].getDirection(), Cart.CART_DIR_DOWN)
+        self.assertEqual(carts[16].getPos(), (143, 129))
+
+        #move one tick
+        self.tick(tracks, carts, 1)
+        self.assertEqual(carts[0].getDirection(), Cart.CART_DIR_UP)
+        self.assertEqual(carts[0].getPos(), (61, 4))
+
+        self.assertEqual(carts[10].getDirection(), Cart.CART_DIR_LEFT)
+        self.assertEqual(carts[10].getPos(), (34, 95))
+        
+        self.assertEqual(carts[9].getDirection(), Cart.CART_DIR_LEFT)
+        self.assertEqual(carts[9].getPos(), (106, 73))
+
+        self.assertEqual(carts[16].getDirection(), Cart.CART_DIR_DOWN)
+        self.assertEqual(carts[16].getPos(), (143, 130))
+
+        #total ticks 2
+        self.tick(tracks, carts, 1)
+        self.assertEqual(carts[0].getDirection(), Cart.CART_DIR_UP)
+        self.assertEqual(carts[0].getPos(), (61, 3))
+        
+       
+        self.assertEqual(carts[9].getDirection(), Cart.CART_DIR_LEFT)
+        self.assertEqual(carts[9].getPos(), (105, 73))
+
+        self.assertEqual(carts[11].getDirection(), Cart.CART_DIR_DOWN)
+        self.assertEqual(carts[10].getPos(), (33, 95))
+
+        self.assertEqual(carts[16].getDirection(), Cart.CART_DIR_DOWN)
+        self.assertEqual(carts[16].getPos(), (143, 131))
+
+        #total ticks 3
+        self.tick(tracks, carts, 1)
+        self.assertEqual(carts[0].getDirection(), Cart.CART_DIR_LEFT)
+        self.assertEqual(carts[0].getPos(), (61, 2))
+       
+        self.assertEqual(carts[9].getDirection(), Cart.CART_DIR_LEFT)
+        self.assertEqual(carts[9].getPos(), (104, 73))
+
+        self.assertEqual(carts[10].getDirection(), Cart.CART_DIR_DOWN)
+        self.assertEqual(carts[10].getPos(), (33, 96))        
+
+        self.assertEqual(carts[16].getDirection(), Cart.CART_DIR_DOWN)
+        self.assertEqual(carts[16].getPos(), (143, 132))
+
+        #total ticks 23
+        self.tick(tracks, carts, 20)
+        self.assertEqual(carts[0].getDirection(), Cart.CART_DIR_UP)
+        self.assertEqual(carts[0].getPos(), (41, 2))
+        
+        self.assertEqual(carts[9].getDirection(), Cart.CART_DIR_LEFT)
+        self.assertEqual(carts[9].getPos(), (88, 69))
+
+        self.assertEqual(carts[10].getDirection(), Cart.CART_DIR_DOWN)
+        self.assertEqual(carts[10].getPos(), (27, 110))  
+
+        self.assertEqual(carts[16].getDirection(), Cart.CART_DIR_LEFT)
+        self.assertEqual(carts[16].getPos(), (135, 144))
+
+        #total ticks 24
+        self.tick(tracks, carts, 1)
+        self.assertEqual(carts[0].getDirection(), Cart.CART_DIR_RIGHT)
+        self.assertEqual(carts[0].getPos(), (41, 1))
+
+        self.assertEqual(carts[10].getDirection(), Cart.CART_DIR_DOWN)
+        self.assertEqual(carts[10].getPos(), (27, 111))  
+
+        self.assertEqual(carts[16].getDirection(), Cart.CART_DIR_LEFT)
+        self.assertEqual(carts[16].getPos(), (134, 144))
+        
+       
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
